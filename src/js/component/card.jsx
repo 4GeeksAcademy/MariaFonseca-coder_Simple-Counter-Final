7eb18765ccaae7fb0b053bd799a245f2115a98e8
María José Fonseca Castro <mfonsecac052@gmail.com>
2024-10-28 02:49:24 +0000

    avance 2

--- src/js/component/card.jsx
+++ src/js/component/card.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 
 const Card = (props) => {
+    const { digitOne, digitTwo, digitThree, digitFour, digitFive, digitSix } = props;
     return (
         <div className="row text-center">
             <div className="col-sm-2">
@@ -10,32 +11,32 @@ const Card = (props) => {
             </div>
             <div className="col-sm-2">
                 <div className="card">
-                    <div className="card-body">Digito 2</div>
+                    <div className="card-body">{props.digitSix}</div>
                 </div>
             </div>
             <div className="col-sm-2">
                 <div className="card">
-                    <div className="card-body">Digito 3</div>
+                    <div className="card-body">{props.digitFive}</div>
                 </div>
             </div>
             <div className="col-sm-2">
                 <div className="card">
-                    <div className="card-body">Digito 4</div>
+                    <div className="card-body">{props.digitFour}</div>
                 </div>
             </div>
             <div className="col-sm-2">
                 <div className="card">
-                    <div className="card-body">Digito 5</div>
+                    <div className="card-body">{props.digitThree}</div>
                 </div>
             </div>
             <div className="col-sm-2">
                 <div className="card">
-                    <div className="card-body">Digito 6</div>
+                    <div className="card-body">{props.digitTwo}</div>
                 </div>
             </div>
             <div className="col-sm-2">
                 <div className="card">
-                    <div className="card-body">Digito 7</div>
+                    <div className="card-body">{props.digitOne}</div>
                 </div>
             </div>
         </div>
--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,12 +1,58 @@
 import React from "react";
+import PropTypes from "prop-types";
 import Card from "./card.jsx";
 
-const Home = () => {
-	return (
-		<div className="container">
-			<Card />
-		</div>
-	);
+// Definimos los tipos de datos esperados para las props de Card
+Card.propTypes = {
+	digitSix: PropTypes.number,
+	digitFive: PropTypes.number,
+	digitFour: PropTypes.number,
+	digitThree: PropTypes.number,
+	digitTwo: PropTypes.number,
+	digitOne: PropTypes.number,
 };
+
+// Componente Home como clase
+class Home extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = { sec: 0 }; // Estado inicial del contador en 0
+	}
+
+	// Inicia el contador cuando el componente se monta
+	componentDidMount() {
+		this.intervalId = setInterval(() => {
+			this.setState((prevState) => ({ sec: prevState.sec + 1 }));
+		}, 1000);
+	}
+
+	// Limpia el intervalo cuando el componente se desmonte
+	componentWillUnmount() {
+		clearInterval(this.intervalId);
+	}
+
+	render() {
+		// Calcula cada dígito en base al valor de `sec` en el estado
+		const one = Math.floor((this.state.sec / 1) % 10);
+		const two = Math.floor((this.state.sec / 10) % 10);
+		const three = Math.floor((this.state.sec / 100) % 10);
+		const four = Math.floor((this.state.sec / 1000) % 10);
+		const five = Math.floor((this.state.sec / 10000) % 10);
+		const six = Math.floor((this.state.sec / 100000) % 10);
+
+		return (
+			<div className="container">
+				<Card
+					digitOne={one}
+					digitTwo={two}
+					digitThree={three}
+					digitFour={four}
+					digitFive={five}
+					digitSix={six}
+				/>
+			</div>
+		);
+	}
+}
 
 export default Home;
