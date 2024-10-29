fe1dea0a88974add181be30d83dbe77727b54ab5
María José Fonseca Castro <mfonsecac052@gmail.com>
2024-10-29 03:05:14 +0000

    correccion terminada

--- src/js/component/card.jsx
+++ src/js/component/card.jsx
@@ -4,56 +4,58 @@ const Card = (props) => {
     const { digitOne, digitTwo, digitThree, digitFour, digitFive, digitSix } = props;
 
     return (
-        <div className="row text-center no-gutters pt-1 pb-1 rounded mt-3" style={{ background: ' #f4c2c2', fontSize: '40px', border: '2px solid #D8BFD8' }}>
-            <div className="col-auto">
-                <div className="card" style={{ width: '50px', height: '70px' }}>
-                    <div className="card-body" style={{ padding: '5px' }}>
-                        <i className="far fa-clock"></i>
+        <div id="container" className="d-flex justify-content-center">
+            <div className="row text-center no-gutters pt-1 pb-1 rounded mt-3" style={{ background: ' #f4c2c2', fontSize: '40px', border: '2px solid #D8BFD8' }}>
+                <div className="col-auto">
+                    <div className="card" style={{ width: '50px', height: '70px' }}>
+                        <div className="card-body" style={{ padding: '5px' }}>
+                            <i className="far fa-clock"></i>
+                        </div>
                     </div>
                 </div>
-            </div>
-            <div className="col-auto">
-                <div className="card" style={{ width: '50px', height: '70px' }}>
-                    <div className="card-body" style={{ padding: '5px' }}>
-                        {digitSix}
+                <div className="col-auto">
+                    <div className="card" style={{ width: '50px', height: '70px' }}>
+                        <div className="card-body" style={{ padding: '5px' }}>
+                            {digitSix}
+                        </div>
                     </div>
                 </div>
-            </div>
-            <div className="col-auto">
-                <div className="card" style={{ width: '50px', height: '70px' }}>
-                    <div className="card-body" style={{ padding: '5px' }}>
-                        {digitFive}
+                <div className="col-auto">
+                    <div className="card" style={{ width: '50px', height: '70px' }}>
+                        <div className="card-body" style={{ padding: '5px' }}>
+                            {digitFive}
+                        </div>
                     </div>
                 </div>
-            </div>
-            <div className="col-auto">
-                <div className="card" style={{ width: '50px', height: '70px' }}>
-                    <div className="card-body" style={{ padding: '5px' }}>
-                        {digitFour}
+                <div className="col-auto">
+                    <div className="card" style={{ width: '50px', height: '70px' }}>
+                        <div className="card-body" style={{ padding: '5px' }}>
+                            {digitFour}
+                        </div>
                     </div>
                 </div>
-            </div>
-            <div className="col-auto">
-                <div className="card" style={{ width: '50px', height: '70px' }}>
-                    <div className="card-body" style={{ padding: '5px' }}>
-                        {digitThree}
+                <div className="col-auto">
+                    <div className="card" style={{ width: '50px', height: '70px' }}>
+                        <div className="card-body" style={{ padding: '5px' }}>
+                            {digitThree}
+                        </div>
                     </div>
                 </div>
-            </div>
-            <div className="col-auto">
-                <div className="card" style={{ width: '50px', height: '70px' }}>
-                    <div className="card-body" style={{ padding: '5px' }}>
-                        {digitTwo}
+                <div className="col-auto">
+                    <div className="card" style={{ width: '50px', height: '70px' }}>
+                        <div className="card-body" style={{ padding: '5px' }}>
+                            {digitTwo}
+                        </div>
                     </div>
                 </div>
-            </div>
-            <div className="col-auto">
-                <div className="card" style={{ width: '50px', height: '70px' }}>
-                    <div className="card-body" style={{ padding: '5px' }}>
-                        {digitOne}
+                <div className="col-auto">
+                    <div className="card" style={{ width: '50px', height: '70px' }}>
+                        <div className="card-body" style={{ padding: '5px' }}>
+                            {digitOne}
+                        </div>
                     </div>
                 </div>
-            </div>
+            </div >
         </div >
     );
 };
--- src/js/index.js
+++ src/js/index.js
@@ -7,7 +7,7 @@ import Card from "./component/card.jsx";
 import "../styles/index.css";
 
 let sec = 0;
-setInterval(() => {
+const interval= setInterval(() => {
 	const one = Math.floor((sec / 1) % 10);
 	const two = Math.floor((sec / 10) % 10);
 	const three = Math.floor((sec / 100) % 10);
@@ -16,4 +16,7 @@ setInterval(() => {
 	const six = Math.floor((sec / 100000) % 10);
     sec++;
     ReactDOM.createRoot(document.getElementById('app')).render(<Card digitOne={one} digitTwo={two} digitThree={three} digitFour={four} digitFive={five} digitSix={six} />);
+    if (sec>100) {
+        clearInterval(interval)
+    }
 }, 1000);
